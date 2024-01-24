9296f0ee987730ed4e9e841db9e368ae7d35d7cd
Sandra Mourali <sandra.mourali@insat.ucar.tn>
2024-01-24 19:16:58 +0100

    fix

--- app/src/main/java/com/example/ProjetAndroid/ViewModel/viewmodels/UserPageViewModel.kt
+++ app/src/main/java/com/example/ProjetAndroid/ViewModel/viewmodels/UserPageViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.ProjetAndroid.Model.api.ApiService.Companion.AUTH_TOKEN
 import com.example.ProjetAndroid.Model.DTOs.queries.GetUser
+import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 
 class UserPageViewModel @AssistedInject constructor(
     private val getUser: GetUser,
     private val sharedPreferences: SharedPreferences,
+    @Assisted private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     private val token = sharedPreferences.getString(AUTH_TOKEN, "")
